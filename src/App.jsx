3ef7db2d7b3d1f2d549da0b1a22243d6f9a2a83a
NGaero <marcos@ngaeroinstruccion.com>
2026-06-05 15:38:56 -0300

    Add aircraft profile images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import StatusCard from './components/StatusCard'
+import a320Perfil from './assets/A320 perfil.png'
 
+import b737Perfil from './assets/b737 perfil.png'
 import EnvelopeChart from './components/EnvelopeChart'
 
 import aircraftDatabase from './data/aircraftDatabase'
