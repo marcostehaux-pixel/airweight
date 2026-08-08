103b184d7a3a01cde318258c9a18bbd44e686213
NGaero <marcos@ngaeroinstruccion.com>
2026-08-08 12:46:13 -0300

    Update login authentication and user access

--- src/Login.jsx
+++ src/Login.jsx
@@ -17,7 +17,7 @@ setShowPassword
 ] = useState(false)
   function login() {
 
-    const expiration = new Date('2026-07-30')
+    const expiration = new Date('2026-08-20')
 
     if (new Date() > expiration) {
 
